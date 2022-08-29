9ea6f900976f33e1d5643f802b7b8ea7ec5f089f
selffy <self-uong15@naver.com>
2022-08-29 17:55:07 +0900

    경주마 게임

---
+++ HorseGame/src/main/webapp/js/horsegame.js
@@ -0,0 +1,46 @@
+var rate =3;
+
+var horse = new Array(4);
+
+horse[0] = "말1";
+horse[1] = "말2";
+horse[2] = "말3";
+horse[3] = "말4";
+
+var speed = new Array(4);
+
+speed[0] = new Array(4);
+speed[1] = new Array(4);
+speed[2] = new Array(4);
+speed[3] = new Array(4);
+
+var rally = new Array(4);
+
+rally[0] = 0;
+rally[1] = 0;
+rally[2] = 0;
+rally[3] = 0;
+
+var goal = 1400;
+
+var goal1 = goal/ 4;
+var goal2 = goal/2 ;
+var goal3 = goal*3/4;
+
+var gage = 0;
+var best =0;
+var j = 0;
+
+var player = new Array(4);
+var rac = new Array(4);
+var jump = 0;
+
+var winner = 0;
+
+player[0] = document.getElementById("horse1");
+player[1] = document.getElementById("horse2");
+player[2] = document.getElementById("horse3");
+player[3] = document.getElementById("horse4");
+
+function start(){
+}
